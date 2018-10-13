bf51fb63036a0195a0c030f6d1386ea54a84e5bc
David Lam <davidlys@gmail.com>
2018-10-13 23:37:50 +0800

    Introduce SearchBar as component

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <form>
+    <input type="text" placeholder="Search..." />
+    <p>
+      <input type="checkbox" />
+      Only show products in stock
+    </p>
+  </form>
+</template>
+
+<script>
+export default {
+
+}
+</script>
