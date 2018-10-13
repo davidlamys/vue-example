0fb25ab62dcde50e4655646d0939b63343d46d0e
David Lam <davidlys@gmail.com>
2018-10-13 19:03:30 +0800

    Wrap product row in component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,35 @@
 <template>
   <div id="app">
+    <table>
+      <product-row v-for = "item in items"
+            :name="item.name"
+            :price="item.price"
+            :stocked="item.stocked">
+      </product-row>
+    </table>
   </div>
 </template>
 
 <script>
 
+import ProductRow from './components/ProductRow.vue'
+
 export default {
   name: 'app',
   components: {
+    ProductRow
+  },
+  data () {
+    return {
+      items: [
+        {category: "Sporting Goods", price: "$49.99", stocked: true, name: "Football"},
+        {category: "Sporting Goods", price: "$9.99", stocked: true, name: "Baseball"},
+        {category: "Sporting Goods", price: "$29.99", stocked: false, name: "Basketball"},
+        {category: "Electronics", price: "$99.99", stocked: true, name: "iPod Touch"},
+        {category: "Electronics", price: "$399.99", stocked: false, name: "iPhone 5"},
+        {category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7"}
+      ]
+    }
   }
 }
 </script>
---
+++ src/components/ProductRow.vue
@@ -0,0 +1,17 @@
+<template>
+    <tr>
+        <td> <span :style="{color: this.color }"> {{ this.name }}</span> </td>
+        <td> {{ price }} </td>
+    </tr>
+</template>
+
+<script>
+export default {
+  props: ['name', 'price', 'stocked'],
+  computed: {
+    color() {
+      return this.stocked ? "red" : "black"
+    }
+  }
+}
+</script>
